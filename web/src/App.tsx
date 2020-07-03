a533177909716cf9ff8f2831e1aae979718675e6
FeruYasu <30644819+FeruYasu@users.noreply.github.com>
2020-07-03 09:31:45 -0300

    web: react initial setup

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <h1>FastFeet</h1>
+    <GlobalStyle />
+  </>
+);
+
+export default App;
